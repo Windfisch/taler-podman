ecf3e5d852dbf677038aff8010fb4be721dc4d80
Florian Jung <flo@windfis.ch>
2022-12-04 19:17:02 +0100

    exchange config gen

--- make_env.sh
+++ make_env.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-cat <<-FNORD
+echo Creating .env
+
+cat <<-FNORD >> .env
 export POSTGRES_PASSWORD='`pwgen -s 20`'
 export SANDBOX_ADMIN_PASSWORD='`pwgen -s 20`'
 export SANDBOX_DB_PASSWORD='`pwgen -s 20`'
 export NEXUS_DB_PASSWORD='`pwgen -s 20`'
 export NEXUS_ADMIN_PASSWORD='`pwgen -s 20`'
 export NEXUS_TALER_PASSWORD='`pwgen -s 20`'
+export EXCHANGE_DB_PASSWORD='`pwgen -s 20`'
+export MYCURRENCY=MANA
+export MYIBAN=DE827320
 FNORD
+
+. .env
+
+rm -rf exchange-config
+cp -r exchange-config.skel exchange-config
+find exchange-config -type f | xargs sed -i "s/MYCURRENCY/$MYCURRENCY/g"
+find exchange-config -type f | xargs sed -i "s/MYIBAN/$MYIBAN/g"
+find exchange-config -type f | xargs sed -i "s/NEXUS_TALER_PASSWORD/$NEXUS_TALER_PASSWORD/g"
+find exchange-config -type f | xargs sed -i "s/EXCHANGE_DB_PASSWORD/$EXCHANGE_DB_PASSWORD/g"
