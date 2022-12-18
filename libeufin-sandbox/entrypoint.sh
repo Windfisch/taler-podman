95d690fd3f6df98508f9beb53c6e39471078fdf6
Florian Jung <flo@windfis.ch>
2022-12-18 23:32:39 +0000

    Set default exchange, change debt limit

--- libeufin-sandbox/entrypoint.sh
+++ libeufin-sandbox/entrypoint.sh
@@ -1,4 +1,5 @@
 export LIBEUFIN_SANDBOX_PORT=5016
-libeufin-sandbox config --currency MANA default
+libeufin-sandbox config --currency MANA default --users-debt-limit 0
+libeufin-sandbox default-exchange $EXCHANGE_URL $PAYTO_URL
 ./one_time_setup.sh &
 libeufin-sandbox serve --port $LIBEUFIN_SANDBOX_PORT --no-localhost-only
