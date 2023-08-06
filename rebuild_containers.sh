2010c2d8e3400f08001449a36a4c502add3dda81
Florian Jung <flo@windfis.ch>
2023-08-06 21:52:36 +0000

    rebuild_containers.sh: fail on error

--- rebuild_containers.sh
+++ rebuild_containers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in libeufin-{base,sandbox,nexus} taler-exchange-insecure taler-merchant; do
-	docker build --tag "$i"':'latest -f $i.Dockerfile .
+	docker build --tag "$i"':'latest -f $i.Dockerfile . || exit 1
 done
